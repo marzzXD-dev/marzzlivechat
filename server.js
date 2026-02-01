22c84f7a99741cbc059ce303dcf83d6422d4cba3
marzzXD-dev <beryymarzz@gmail.com>
2026-02-01 21:13:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,8 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
+
 });
